0c7845851c5261f96a267b4688d2cc8e893ee251
Milan V. Kecojević <staog@gmx.com>
2019-03-31 12:15:10 +0200

    Add files via upload

---
+++ basic-javascript/Return a Value from a Function with Return.js
@@ -0,0 +1,16 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+
+
+console.log(minusSeven(10));
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(3));
---
+++ basic-javascript/Understanding Undefined Value returned from a Function.js
@@ -0,0 +1,16 @@
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+
+
+// Only change code above this line
+var returnedValue = addFive();
+
+function addFive() {
+  sum = sum + 5;
+}
