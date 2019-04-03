113e5164a633c5609cd93d6e9e7bca94095ba262
Milan V. Kecojević <staog@gmx.com>
2019-04-03 20:40:08 +0200

    Add files via upload

---
+++ ES6/Write Higher Order Arrow Functions.js
@@ -0,0 +1,11 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = realNumberArray.filter(item => item > 0 && Number.isInteger(item)).map(item => item * item);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
