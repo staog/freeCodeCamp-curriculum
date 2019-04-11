b4aada9b13672327bcc361eb8b09af862555d9bd
Milan V. Kecojević <staog@gmx.com>
2019-04-11 22:09:05 +0200

    Add files via upload

---
+++ functional-programming/Refactor Global Variables Out of Functions.js
@@ -0,0 +1,35 @@
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (arr, bookName) {
+  let myBookList = [...arr];
+  myBookList.push(bookName);
+  return myBookList;
+  
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (arr, bookName) {
+  let myBookList = [...arr];
+
+  if (myBookList.indexOf(bookName) >= 0) {
+    myBookList.splice(myBookList.indexOf(bookName), 1);
+    return myBookList;
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
