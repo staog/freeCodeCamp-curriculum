e48047b0bcc0e5b0eb7ff2ad62e873f78102d8e0
Milan V. Kecojević <staog@gmx.com>
2019-04-13 23:12:31 +0200

    Add files via upload

---
+++ intermediate-algorithm-scripting/Wherefore art thou.js
@@ -0,0 +1,17 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var srcKeys = Object.keys(source);
+  
+  return collection.filter(function (obj) {
+    for(var i = 0; i < srcKeys.length; i++) {
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
