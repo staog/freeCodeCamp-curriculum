b48d03b2738fe2858d357b413aadf400185c33b4
Milan V. Kecojević <staog@gmx.com>
2019-04-17 21:02:37 +0200

    Create Palindrome Checker.js

---
+++ javascript-algorithms-and-data-structures-projects/Palindrome Checker.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+  let regExp = /[^A-Za-z0-9]/g // or we can use this /[\W_]/g
+  let lowerCaseStr = str.toLowerCase().replace(regExp, '');
+  let reverseString = lowerCaseStr.split('').reverse().join('');
+  
+  if (lowerCaseStr === reverseString) {
+    return true;
+   } else {
+    return false;
+  }
+}
+
+palindrome("eye");
