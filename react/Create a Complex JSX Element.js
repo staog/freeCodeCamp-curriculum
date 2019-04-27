94eadfd044d1329845b21d1b1cbcc6ab1287f54b
Milan V. Kecojević <staog@gmx.com>
2019-04-27 19:12:59 +0200

    Create Create a Complex JSX Element.js

---
+++ react/Create a Complex JSX Element.js
@@ -0,0 +1,11 @@
+// write your code here
+
+const JSX = <div>
+              <h1>React</h1>
+              <p>Is is good for:</p>
+              <ul>
+                <li>Speed?</li>
+                <li>Rendering?</li>
+                <li>Bandwidth?</li>
+              </ul>
+            </div>
