279bb665fc0aae294ffe73f9116b8b91757d9447
Milan V. Kecojević <staog@gmx.com>
2019-04-30 17:09:22 +0200

    Add files via upload

---
+++ react/Render a Class Component to the DOM.js
@@ -0,0 +1,21 @@
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* change code above this line */}
+      </div>
+    );
+  }
+};
+
+// change code below this line
+let target = document.getElementById('challenge-node');
+
+ReactDOM.render(<TypesOfFood />, target);
