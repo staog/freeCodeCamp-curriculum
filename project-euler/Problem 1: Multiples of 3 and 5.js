9184f58a232fcccbfa153e512121b6c95027d34a
Milan V. Kecojević <staog@gmx.com>
2019-05-12 18:26:03 +0200

    Create Problem 1: Multiples of 3 and 5.js

---
+++ project-euler/Problem 1: Multiples of 3 and 5.js
@@ -0,0 +1,12 @@
+function multiplesOf3and5(number) {
+  // Good luck!
+  let sum = 0;
+  for (let i = 3; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+multiplesOf3and5(1000);
