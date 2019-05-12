d79aab3eff281be4f58e29a051be157c9779cfb0
Milan V. Kecojević <staog@gmx.com>
2019-05-12 18:18:27 +0200

    Add files via upload

---
+++ data-structures/Remove from a Set.js
@@ -0,0 +1,32 @@
+function Set() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // this method will add an element to the set
+    this.add = function(element) {
+        if(!this.has(element)){
+            collection.push(element);
+            return true;
+        }
+        return false;
+    };
+    // change code below this line
+    this.remove = function(element) {
+        if (this.has(element)) {
+            let position = collection.indexOf(element);
+            collection.splice(position, 1);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    // change code above this line
+}
